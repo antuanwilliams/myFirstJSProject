92c3ef27c421ceff3330aa143bc30595c979a556
leon williams <antuanwilliams0@gmail.com>
2022-06-28 23:15:29 +0100

    first commit

---
+++ javascript_course/js/scripts.js
@@ -0,0 +1,9 @@
+/* alert("Testing the alert command");
+
+console.log("Testing the console.log command"); */
+
+console.log ( document.getElementById("blue_box").innerHTML );
+
+document.getElementById("blue_box").innerHTML = "<h1>Testing</h1>";
+
+console.log ( document.getElementById("blue_box").innerHTML = "<h1>Testing</h1>" );
