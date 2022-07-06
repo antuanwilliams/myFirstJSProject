ac65529f17d8b5fe7e5147d8a3d8a380a1c7b137
leon williams <antuanwilliams0@gmail.com>
2022-07-06 22:03:23 +0100

    Strings and The Math Object

--- javascript_course/js/scripts.js
+++ javascript_course/js/scripts.js
@@ -2,8 +2,53 @@
 
 console.log("Testing the console.log command"); */
 
-console.log ( document.getElementById("blue_box").innerHTML );
+/* ********************************
+Lesson 4 - working with the DOM 
+********************************* */
 
-document.getElementById("blue_box").innerHTML = "<h1>Testing</h1>";
+// console.log ( document.getElementById("blue_box").innerHTML );
 
-console.log ( document.getElementById("blue_box").innerHTML = "<h1>Testing</h1>" );
+// document.getElementById("blue_box").innerHTML = "<h1>Testing</h1>";
+
+// console.log ( document.getElementById("blue_box").innerHTML = "<h1>Testing</h1>" );
+
+/* ********************************
+Lesson 5 - Variables
+********************************* */
+
+// var user_name = "Leon"
+
+// document.getElementById("user_greeting_message").innerHTML = "Hello " + user_name + "!";
+
+/* ********************************
+Lesson 6 - dataTypes - strings
+********************************* */
+
+/* var firstName = "Leon";
+console.log(firstName.length);
+console.log(firstName[3]);
+
+var url = "https://www.google.com";
+console.log(url.replace("https://" , " ")); */
+
+/* ********************************
+Lesson 7 - dataTypes - numbers
+********************************* */
+
+var num1 = 7;
+var num2 = 3;
+console.log(num1 + num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 - num2);
+
+console.log(Math.pow(2,4));
+console.log(Math.round(3.5));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(6.9));
+console.log(Math.sqrt(36));
+
+console.log(Math.random());
+console.log(Math.random()*100);
+console.log(Math.round(Math.random()*100));
+
